33eeec4dbf2b341f48fefebcabb9e40433934e2f
kozueiwasaki <kozutan1995@gmail.com>
2020-05-28 23:44:19 +0900

    insert data to departments-table

--- src/api/db/data/departments.php
+++ src/api/db/data/departments.php
@@ -2,18 +2,19 @@
 
 include '../../setting.php';
 
-$sql = "INSERT INTO departments(name)
+$sql = "INSERT INTO departments (name)
 VALUES
 ('総務部'),
 ('経理部'),
 ('業務部'),
 ('情報システム部'),
 ('商品部'),
-('営業部')
+('営業部');
 ";
 
 $res = pg_query($db, $sql);
-
+print($sql);
+print($res);
 if ($res){
   print('success');
 }else{
