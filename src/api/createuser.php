8b7d28cb277833a4de47505d659519e4bb54ca10
kozueiwasaki <kozutan1995@gmail.com>
2020-06-01 08:46:59 +0900

    ユーザー登録機能の大枠

--- src/api/createuser.php
+++ src/api/createuser.php
@@ -1,49 +1,25 @@
 <?php
-
 //データベースへ接続
 include 'setting(pdo).php';
 
-
-// //POSTのValidate。
-// if (!$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-//     echo '入力された値が不正です。';
-//     return false;
-//   }
-
-//   //パスワードの正規表現
-//   if (preg_match('/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i', $_POST['password'])) {
-//     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-//   } else {
-//     echo 'パスワードは半角英数字をそれぞれ1文字以上含んだ8文字以上で設定してください。';
-//     return false;
-//   }
-
-//   //登録処理
-//   try {
-//     $stmt = $pdo->prepare("insert into userDeta(email, password) value(?, ?)");
-//     $stmt->execute([$email, $password]);
-//     echo '登録完了';
-//   } catch (\Exception $e) {
-//     echo '登録済みのメールアドレスです。';
-//   }
-  
-  /*
-  //DB内のメールアドレスを取得
-  $stmt = $pdo->prepare("select email from userDeta where email = ?");
-  $stmt->execute([$email]);
-  $row = $stmt->fetch(PDO::FETCH_ASSOC);
-  //DB内のメールアドレスと重複していない場合、登録する。
-  if (!isset($row['email'])) {
-    $stmt = $pdo->prepare("insert into userDeta(email, password) value(?, ?)");
-    $stmt->execute([$email, $password]);
-    echo "登録完了";
-  } else {
-    echo '既に登録されたメールアドレスです。';
-    return false;
-  }
-  */
-
-
-
+//メールアドレスの重複をチェック
+$user = $db->prepare('SELECT COUNT(*) AS cnt FROM users WHERE email=?');
+$user->execute(array($_POST['email'] . 'rvp.co.jp'));
+$record = $user->fetch();
+if($record['cnt'] > 0){
+  $error['email'] = 'duplicate';
+  return false;
+}else{
+  // 重複がなければ登録する
+  $statement = $db->prepare('INSERT INTO users SET name=?, department_id=?, email=?, password=?');
+  $statement->execute(array(
+    $_POST['name'],
+    $_POST['department_id'],
+    $_POST['email'] . 'rvp.co.jp',
+    sha1($_POST['password'])
+  ));
+  // header('Location: ユーザー一覧画面');
+  exit();
+}
 
 ?>
