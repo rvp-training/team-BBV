be4833683435553ee2f4153d78dac4938ec22ec6
kozueiwasaki <kozutan1995@gmail.com>
2020-06-02 11:10:31 +0900

    view側にコード書いてるので使わないかも

--- src/api/login.php
+++ src/api/login.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /*
 if (isset($_SESSION['EMAIL'])) {
   echo 'ようこそ' .  h($_SESSION['EMAIL']) . "さん<br>";
@@ -9,29 +10,29 @@ if (isset($_SESSION['EMAIL'])) {
 */
 
 
-//DB内でPOSTされたメールアドレスを検索
-try {
-    $pdo = include 'setting.php';
-    $stmt = $pdo->prepare('select * from userDeta where email = ?');
-    $stmt->execute([$_POST['email']]);
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-  } catch (\Exception $e) {
-    echo $e->getMessage() . PHP_EOL;
-  }
+// //DB内でPOSTされたメールアドレスを検索
+// try {
+//     $pdo = include 'setting.php';
+//     $stmt = $pdo->prepare('select * from userDeta where email = ?');
+//     $stmt->execute([$_POST['email']]);
+//     $row = $stmt->fetch(PDO::FETCH_ASSOC);
+//   } catch (\Exception $e) {
+//     echo $e->getMessage() . PHP_EOL;
+//   }
 
-  //emailがDB内に存在しているか確認
-if (!isset($row['email'])) {
-    echo 'メールアドレス又はパスワードが間違っています。';
-    return false;
-  }
+//   //emailがDB内に存在しているか確認
+// if (!isset($row['email'])) {
+//     echo 'メールアドレス又はパスワードが間違っています。';
+//     return false;
+//   }
 
-  //パスワード確認後sessionにメールアドレスを渡す
-if (password_verify($_POST['password'], $row['password'])) {
-    session_regenerate_id(true); //session_idを新しく生成し、置き換える
-    $_SESSION['EMAIL'] = $row['email'];
-    echo 'ログインしました';
-  } else {
-    echo 'メールアドレス又はパスワードが間違っています。';
-    return false;
-  }
+//   //パスワード確認後sessionにメールアドレスを渡す
+// if (password_verify($_POST['password'], $row['password'])) {
+//     session_regenerate_id(true); //session_idを新しく生成し、置き換える
+//     $_SESSION['EMAIL'] = $row['email'];
+//     echo 'ログインしました';
+//   } else {
+//     echo 'メールアドレス又はパスワードが間違っています。';
+//     return false;
+//   }
 ?>
