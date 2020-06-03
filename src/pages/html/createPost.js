37eb50ddd82943ddc417db5f3b2aab984d48ffc3
nuidananako <nuida.nanako@revamp.co.jp>
2020-06-03 09:07:37 +0900

    updateFile

--- src/pages/html/createPost.js
+++ src/pages/html/createPost.js
@@ -1,23 +1,88 @@
-$(function(){
+$(function() {
 
-    // //カウントするフィールドを監視
-    // $('#title').keyup(function(){
- 
-    //     //現在入力されている文字
-    //     var text = $('#title').val();
- 
-    //     //現在の文字数
-    //     var count = text.length;
- 
-    //     //処理分け
-    //     if(count == 0 ){
-    //         //字を消して0文字となった場合。
-    //         $('#send').prop('disabled', true);
 
-    //     } else {
-    //         $('#send').prop('disabled', false);
-    //     }
-    // })
+    $('.input-0').on('change', function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(".img-0").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
 
-    
-});
+    $('.input-1').on('change', function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(".img-1").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+
+    $('.input-2').on('change', function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(".img-2").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+
+    $('.input-3').on('change', function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(".img-3").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+
+    $('.input-4').on('change', function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(".img-4").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+
+    $('.input-5').on('change', function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(".img-5").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+
+    $('.input-6').on('change', function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(".img-6").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+
+    $('.input-7').on('change', function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(".img-7").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+
+    $('.input-8').on('change', function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(".img-8").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+
+    $('.input-9').on('change', function (e) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(".img-9").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    });
+
+
+
+
+  });
+
