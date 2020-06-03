04de00c0f2bb39b9ac868ab0aab1ae2ba849dd07
akiche <suzuki.akito.@revamp.co.jp>
2020-06-03 18:37:12 +0900

    a

--- src/api/deleteuser.php
+++ src/api/deleteuser.php
@@ -1,6 +1,9 @@
 <?php
 
-require('setting.php');
+session_start();
+include('setting.php');
+
+//require('setting.php');
 
 $sql = "delete from users where id=$id";
 if(!$res=mysql_query($sql)){
--- src/pages/html/admin/deleteUser.php
+++ src/pages/html/admin/deleteUser.php
@@ -2,6 +2,14 @@
 session_start();
 include '../../../api/setting.php';
 
+// myposts.phpの５－９行目をコピーし、
+// パラメータを受け取る
+$id = $_REQUEST['id'];
+if (!is_numeric($id) || $id <= 0) {
+  print('パラメータは1以上の数字で指定してください');
+  exit();
+}
+
 // ログインしている、かつ管理者であるかチェック
 // 一般ユーザ用ログインページにリダイレクト
 $sql =$db->prepare('SELECT * FROM users WHERE id=?');
@@ -10,6 +18,15 @@ $result = $sql->fetch();
 if (!isset($_SESSION['id']) || !$result['is_admin']) {
     header('Location: ../login.php');
 }
+
+//初期値を入れる
+//受け取ったidを引数にして、getUserInfoを呼び出し値を取得
+//結果をｈｔｍｌの中に埋め込む
+include "../../../api/getuserinfo.php";
+$user = new User();
+$userinfo = $user->getUserInfo($id);
+$_SESSION['selected'] = $id;
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -38,8 +55,8 @@ if (!isset($_SESSION['id']) || !$result['is_admin']) {
 
         <!--コンテンツ-->
         <div id="getContents">
-            <p>ユーザー名　：　"ここにGET"</p>
-            <p>所属部署　：　"ここにGET"</p>
+            <p>ユーザー名　：<?php echo $userinfo['name']; ?> </p>
+            <p>所属部署　：　<?php echo $userinfo['department']; ?></p>
             
             <div id="editButton">
                 <p>このユーザーを本当に削除してよろしいですか？<span class="br">削除しても投稿やコメントは残ります。</span></p>
