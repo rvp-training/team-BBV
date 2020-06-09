4990e696319d3b5bf049ff98c691bf34b5fa2484
kozueiwasaki <kozutan1995@gmail.com>
2020-06-09 13:33:24 +0900

    gitignoreを反映

--- src/api/updateuser.php
+++ src/api/updateuser.php
@@ -2,11 +2,13 @@
 session_start();
 include('setting.php');
 
+$image_name = $_SESSION['id'] . ".jpeg";
+
 $stmt = $db->prepare('UPDATE users 
-                        SET introduction = :introduction 
+                        SET introduction = :introduction, image = :image
                         WHERE id = :id;');
 
-$result = $stmt->execute(array(':introduction' => $_POST['introduction'], ':id' => $_SESSION['id']));
+$result = $stmt->execute(array(':introduction' => $_POST['introduction'], ':image' => $image_name, ':id' => $_SESSION['id']));
 
 
 if($result){
@@ -15,6 +17,6 @@ if($result){
     echo "更新に失敗しました";
 }
 
-move_uploaded_file($_FILES['image']['tmp_name'], "../images/users/".$_SESSION['id'].".jpeg");
+move_uploaded_file($_FILES['image']['tmp_name'], "../images/users/" . $image_name);
 
 ?>
