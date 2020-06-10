c764b14ac47b53a79f4a8fb627a07269eac472ac
kozueiwasaki <kozutan1995@gmail.com>
2020-06-10 15:15:20 +0900

    ユーザー情報編集機能の修正

--- src/api/updateuser.php
+++ src/api/updateuser.php
@@ -2,13 +2,23 @@
 session_start();
 include('setting.php');
 
-$image_name = $_SESSION['id'] . ".jpeg";
-
-$stmt = $db->prepare('UPDATE users 
+if($_FILES['image']['tmp_name']){
+    
+    $image_name = $_SESSION['id'] . ".jpeg";
+    $stmt = $db->prepare('UPDATE users 
                         SET introduction = :introduction, image = :image
                         WHERE id = :id;');
+    $result = $stmt->execute(array(':introduction' => $_POST['introduction'], ':image' => $image_name, ':id' => $_SESSION['id']));
+    move_uploaded_file($_FILES['image']['tmp_name'], "../images/users/" . $image_name);
 
-$result = $stmt->execute(array(':introduction' => $_POST['introduction'], ':image' => $image_name, ':id' => $_SESSION['id']));
+} else {
+
+    $stmt = $db->prepare('UPDATE users 
+                        SET introduction = :introduction
+                        WHERE id = :id;');
+    $result = $stmt->execute(array(':introduction' => $_POST['introduction'], ':id' => $_SESSION['id']));
+    
+}
 
 
 if($result){
@@ -17,6 +27,5 @@ if($result){
     echo "更新に失敗しました";
 }
 
-move_uploaded_file($_FILES['image']['tmp_name'], "../images/users/" . $image_name);
 
 ?>
