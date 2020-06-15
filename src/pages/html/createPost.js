37da83e91b0f997dc7ce39cc97663955fdce61cd
nuidananako <nuida.nanako@revamp.co.jp>
2020-06-15 17:43:38 +0900

    img-delete

--- src/pages/html/createPost.js
+++ src/pages/html/createPost.js
@@ -82,7 +82,55 @@ $(function() {
     });
 
 
+    $('.delete-0').on('click', function () {
+        $('.input-0').val('');
+        $('.img-0').attr('src', '');
+    });
 
+    $('.delete-1').on('click', function () {
+        $('.input-1').val('');
+        $('.img-1').attr('src', '');
+    });
 
-  });
+    $('.delete-2').on('click', function () {
+        $('.input-2').val('');
+        $('.img-2').attr('src', '');
+    });
+
+    $('.delete-3').on('click', function () {
+        $('.input-3').val('');
+        $('.img-3').attr('src', '');
+    });
+
+    $('.delete-4').on('click', function () {
+        $('.input-4').val('');
+        $('.img-4').attr('src', '');
+    });
+
+    $('.delete-5').on('click', function () {
+        $('.input-5').val('');
+        $('.img-5').attr('src', '');
+    });
+
+    $('.delete-6').on('click', function () {
+        $('.input-6').val('');
+        $('.img-6').attr('src', '');
+    });
+
+    $('.delete-7').on('click', function () {
+        $('.input-7').val('');
+        $('.img-7').attr('src', '');
+    });
+
+    $('.delete-8').on('click', function () {
+        $('.input-8').val('');
+        $('.img-8').attr('src', '');
+    });
+
+    $('.delete-9').on('click', function () {
+        $('.input-9').val('');
+        $('.img-9').attr('src', '');
+    });
+
+});
 
