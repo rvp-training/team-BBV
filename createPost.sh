02cbd69fc853c7a8951cacbb0723363e5567f163
Ubuntu <ubuntu@ip-172-26-15-222.ap-northeast-1.compute.internal>
2020-06-19 07:43:06 +0000

    スクリプト編集

--- createPost.sh
+++ createPost.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 set -e
 set -u
-for ((i=0; i<10; ++i))
+for ((i=0; i<9000; ++i))
 do
+  if [ $(($i%2)) -eq 0 ]; then
     curl http://localhost/api/createPost.php \
     -X POST \
     -H 'Content-Type:multipart/form-data' \
-    -F 'image[]=@/home/ubuntu/team-BBV/screenshot.png' \
+    -F 'image[]=@/home/ubuntu/team-BBV/test_images/screenshot.png' \
+    -F 'image[]=@/home/ubuntu/team-BBV/test_images/screenshot2.png' \
     -F 'workspace=1' \
-    -F 'title=Title' \
+    -F 'title=System' \
     -F "text=hoge$i"
+  else
+     curl http://localhost/api/createPost.php \
+    -X POST \
+    -H 'Content-Type:multipart/form-data' \
+    -F 'image[]=@/home/ubuntu/team-BBV/test_images/maru.jpg' \
+    -F 'image[]=@/home/ubuntu/team-BBV/test_images/poko.jpg' \
+    -F 'workspace=2' \
+    -F 'title=Private' \
+    -F "text=hoge$i"
+  fi
 done
 docker container exec team-bbv_db_1 psql -U postgres -c 'SELECT COUNT(*) AS images FROM images;'
 docker container exec team-bbv_db_1 psql -U postgres -c 'SELECT COUNT(*) AS posts FROM posts;'
